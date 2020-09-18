bf2fb54a3df2f0576ad870596997551cc161c751
Emmanuelle LARZABAL <emmanuelle.larzabal64@gmail.com>
2020-09-18 16:31:18 +0200

    add campus page which contains campus list

--- src/main/java/com/wildcodeschool/myProjectWithSecurity/controllers/HelloWorldController.java
+++ src/main/java/com/wildcodeschool/myProjectWithSecurity/controllers/HelloWorldController.java
@@ -1,22 +1,27 @@
 package com.wildcodeschool.myProjectWithSecurity.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @GetMapping({"/","/home"})
+    @ResponseBody
     public String welcome_SHIELD(){
         return "Welcome to the SHIELD";
     }
 
     @GetMapping("/avengers/assemble")
-    public String welcome_avengers_assemble() {
-        return "Avengers... Assemble";
+    public String welcome_avengers_assemble(Model model) {
+        model.addAttribute("campus", "campus");
+        return "campus";
     }
 
     @GetMapping("/secret-bases")
+    @ResponseBody
     public String welcome_secret_bases() {
         return "secret-bases";
     }
